2733aa08fc1eef93d97f3eb2aebc7736a7131055
Tati <santiagomatheu@gmail.com>
2026-09-15 22:17:45 -0300

    add enum for status on pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -5,7 +5,13 @@ class Pet < ApplicationRecord
   has_many :medical_records, dependent: :destroy
   has_many :adoption_applications, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
+  enum :status, {
+    available: "available",
+    in_process: "in_process",
+    adopted: "adopted"
+  }, default: "available"
+
   validates :name, presence: true
   validates :gender, presence: true
   validates :age_months, presence: true
